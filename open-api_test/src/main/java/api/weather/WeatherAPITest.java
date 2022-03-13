3666550f7f0c9969a7d62dadf59146d3ed79c261
Kukjun Lee <20171655.info@edu.hanbat.ac.kr>
2022-03-13 19:45:25 +0900

    <feature> : <Update open-api_test Project>
    * open api를 사용할때 weather VO 객체의 구조를 수정해서 이를 반영함
    * open api를 사용하면서 있었던 time, date 값을 저장할 수 있도록 수정함.

---
+++ open-api_test/src/main/java/api/weather/WeatherAPITest.java
@@ -0,0 +1,21 @@
+package main.java.api.weather;
+
+import main.java.vo.Weather;
+
+public class WeatherAPITest{
+    public static void main(String[] args) {
+        WeatherAPI api = new WeatherAPI(
+                "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst",
+                "Tk3dN%2FBOZoQkVmZXXo3ZQSKXHvJL4SwU%2FFV8rcF%2BsMsEXFfRuSdcPl6oweXAbVNKD3TiJpBFxBop76XQb45ZFg%3D%3D"
+        );
+
+        try {
+            Weather weather = api.use("69", "100", "20220310", "1800");
+            System.out.println(weather.toString());
+
+        } catch (Exception e) {
+
+        }
+
+    }
+}
