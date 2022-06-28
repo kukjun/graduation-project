d981b65dadbe8d0624605dd3e1373b6130a930b8
Kukjun Lee <lxx3380@gmail.com>
2022-06-28 11:15:53 +0900

    <fix> : <오타 수정>
    - 클래스 이름 오류를 수정
    
    Resolves: #issueNo
    See also: #issueNo

--- src/main/java/io/wisoft/testermatchingplatform/web/quest/QuestListReadController.java
+++ src/main/java/io/wisoft/testermatchingplatform/web/quest/QuestListReadController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class questListReadController {
+public class QuestListReadController {
 
     private final QuestListReadService questListReadService;
 
