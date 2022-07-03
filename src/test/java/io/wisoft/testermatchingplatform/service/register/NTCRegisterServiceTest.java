ea8f3c54d44d99b205df9164bb15324ad0b3517a
Kukjun Lee <lxx3380@gmail.com>
2022-07-03 20:40:51 +0900

    <fix> : <NTC 회원가입 로직 추가 등록>
    - NTC 회원가입 하는 로직 수행 도중 예외처리 부분에서 막힘
    
    Resolves: #issueNo
    See also: #7

--- src/test/java/io/wisoft/testermatchingplatform/service/register/NTCRegisterServiceTest.java
+++ src/test/java/io/wisoft/testermatchingplatform/service/register/NTCRegisterServiceTest.java
@@ -3,8 +3,6 @@ package io.wisoft.testermatchingplatform.service.register;
 import io.wisoft.testermatchingplatform.domain.ntc.NTC;
 import io.wisoft.testermatchingplatform.domain.ntc.NTCEntity;
 import io.wisoft.testermatchingplatform.domain.ntc.NTCRepository;
-import io.wisoft.testermatchingplatform.handler.exception.EmailOverlapException;
-import io.wisoft.testermatchingplatform.handler.exception.NicknameOverlapException;
 import io.wisoft.testermatchingplatform.web.dto.request.NTCRegisterRequest;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -46,7 +44,6 @@ class NTCRegisterServiceTest {
         given(ntcRepository.findById(fakeNTCId))
                 .willReturn(Optional.ofNullable(ntcEntity));
 
-
         //when
         Long newNTCId = ntcRegisterService.registerNTC(request);
 
@@ -55,7 +52,6 @@ class NTCRegisterServiceTest {
 
         NTC ntc = ntcEntity.toDomain();
         NTC findNTC = findNTCEntity.toDomain();
-
 
         assertEquals(ntc.getId(), findNTC.getId());
         assertEquals(ntc.getPassword(), findNTC.getPassword());
@@ -68,9 +64,8 @@ class NTCRegisterServiceTest {
 //    public void ntc_중복_이메일회원_예외발생() {
 //        //given
 //        NTCRegisterRequest request = createNTCRequest();
-//        NTCRegisterRequest overlapRequest = createNTCRequest();
+//        NTCRegisterRequest overlapRequest = createOverlapEmailNTCRequest();
 //        NTCEntity ntcEntity = createNTCEntity(request);
-//        NTCEntity overlapNTCEntity = createNTCEntity(overlapRequest);
 //
 //        Long fakeNTCId = 1L;
 //        ReflectionTestUtils.setField(ntcEntity, "id", fakeNTCId);
@@ -80,12 +75,12 @@ class NTCRegisterServiceTest {
 //                .willReturn(ntcEntity);
 //        given(ntcRepository.findById(fakeNTCId))
 //                .willReturn(Optional.ofNullable(ntcEntity));
-//        Long newNTCId = ntcRegisterService.registerNTC(request);
-//
 //
 //        //when
-//        ntcRegisterService.registerNTC(overlapRequest);
-//
+//        Long newNTCId = ntcRegisterService.registerNTC(request);
+//        assertThrows(EmailOverlapException.class, () ->
+//                ntcRegisterService.registerNTC(overlapRequest)
+//        );
 //
 //    }
 
@@ -103,17 +98,17 @@ class NTCRegisterServiceTest {
                 "kukjun@naver.com", "12341234", "imKing", "010-1234-5678"
         );
     }
-
-    private NTCRegisterRequest createOverlapEmailNTCRequest() {
-        return new NTCRegisterRequest(
-                "kukjun@naver.com", "123414321", "ImJun", "010-1234-4241"
-        );
-    }
-
-    private NTCRegisterRequest createOverlapNicknameNTCRequest() {
-        return new NTCRegisterRequest(
-                "kukjun@naver.com", "12341234", "imKing", "010-1234-5678"
-        );
-    }
+//
+//    private NTCRegisterRequest createOverlapEmailNTCRequest() {
+//        return new NTCRegisterRequest(
+//                "kukjun@naver.com", "123414321", "ImJun", "010-1234-4241"
+//        );
+//    }
+//
+//    private NTCRegisterRequest createOverlapNicknameNTCRequest() {
+//        return new NTCRegisterRequest(
+//                "kukjun@naver.com", "12341234", "imKing", "010-1234-5678"
+//        );
+//    }
 
 }
